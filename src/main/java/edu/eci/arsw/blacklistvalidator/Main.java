8ed9fc8e42bf00286e9f4a162dce288653d333e4
andrea.duran <andrea.duran@mail.escuelaing.edu.co>
2023-08-17 09:55:43 -0500

    Parte 1

--- src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
@@ -12,7 +12,8 @@ import java.util.List;
  * @author hcadavid
  */
 public class Main {
-    
+
+
     public static void main(String a[]){
         HostBlackListsValidator hblv=new HostBlackListsValidator();
         List<Integer> blackListOcurrences=hblv.checkHost("200.24.34.55");
